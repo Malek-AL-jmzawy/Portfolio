22d4b0dddb718a6e23dc7f8b1551a4700e3c4560
malek88888 <jmzomalek@gmail.com>
2021-01-06 14:45:58 +0200

    some changes to the references added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+// const path = require('path'); 
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 import {
   BrowserRouter as Router,
@@ -61,7 +63,7 @@ export default class App extends Component {
           <Route path="/EDUCATION">
           </Route>
           <div className="head about" >
-            <h1>MALIK MUSLEH</h1>
+            <h1>MALIK MUSLEH</h1>  
             <h2>SOFTWARE ENGINEER</h2>
             <a href="mailto:jmzomalek@gmail.com"> &#9993; Jmzomalek@gmail.com</a>
             <a href="tel:962780950198"> &#9990; +96278950198</a>
@@ -72,11 +74,11 @@ export default class App extends Component {
               <li><a href="https://www.linkedin.com/in/malikdarmusleh/" > <img src="https://cdn.iconscout.com/icon/free/png-256/linkedin-2662666-2213265.png" ></img></a></li>
               <li><a href="tel:962780950198"><img src="https://i.pinimg.com/originals/b5/d7/19/b5d7198734ee5174b581630628ecfdd6.png" ></img></a></li>
               <li> <a href="https://github.com/Malek-AL-jmzawy/"><img src="https://icons-for-free.com/iconfiles/png/512/part+1+github-1320568339880199515.png" ></img></a></li>
-              <li> <a href="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=ed6de6839a&attid=0.1&permmsgid=msg-a:r2171258112158663993&th=176c870eb0b34f09&view=att&disp=inline&realattid=176c8704d25c0f7b60e1&saddbat=ANGjdJ9iVbEuPvntspUL4fxnXUINjSUMY4SEspD9TSUxAf4S-y_-Rij7EcKmZFB3UvWhOvIy830gvNHmIRni-SuamYHim5CkhcfO7NPl0K0Ikm9qxXUrHU9v_cl6uqSnQSjL655Taqi_oBR3RaN7jXOw_Amz1aWyMQcP5GCycec9KW6S8FeoynBB3qQaqTplv3xphekEUjYBsOQrw_4h2iHj8_mQXmVZlBai_Q4w92kVyP_tEl4AUcBtshT6Nuuv2oKcqsiGcZLI-9rmc_HAS0BN9uTOQQeYNIh-jkAQSqe0sTaDtZsSeFy4ZJ51kRLLOkzyRhwzqYZxIoVqNKiYR0BTEAMmhB7CjT4tHyJqIT6VLN0lu_K16vJ0g1-76OSaov17xsvmswLdYJovWtAj4ubgENbNMdC7xD2JNRJEgGQxpb1m7AkrKyrqtlYxCSSBQ2RXv4rbo4RISVSKVITPPLhYQcAe-fwa-kX540GLU89kzNi2C6ED4A5xwVrvzO4xyqz73ooEQ3hBQjAp9q8PJDce8Hh59gPktbLyJJLpo1CjMOjFbpcn61_gGZ8EBA-cZUvDO3IlrUx1sg9G6eHnG0aGCZTolEstyBVsgpFCcFQ23COWkI-WxawJMhWHpo9YRp89u0U7jT_4EmmVoqj1wkjtqcQSUPFNVafrY_veIatlx3Dkz3N2OOMFKVrnFnc">
+              <li id="SKILLS"> <a href="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=ed6de6839a&attid=0.1&permmsgid=msg-a:r2171258112158663993&th=176c870eb0b34f09&view=att&disp=inline&realattid=176c8704d25c0f7b60e1&saddbat=ANGjdJ9iVbEuPvntspUL4fxnXUINjSUMY4SEspD9TSUxAf4S-y_-Rij7EcKmZFB3UvWhOvIy830gvNHmIRni-SuamYHim5CkhcfO7NPl0K0Ikm9qxXUrHU9v_cl6uqSnQSjL655Taqi_oBR3RaN7jXOw_Amz1aWyMQcP5GCycec9KW6S8FeoynBB3qQaqTplv3xphekEUjYBsOQrw_4h2iHj8_mQXmVZlBai_Q4w92kVyP_tEl4AUcBtshT6Nuuv2oKcqsiGcZLI-9rmc_HAS0BN9uTOQQeYNIh-jkAQSqe0sTaDtZsSeFy4ZJ51kRLLOkzyRhwzqYZxIoVqNKiYR0BTEAMmhB7CjT4tHyJqIT6VLN0lu_K16vJ0g1-76OSaov17xsvmswLdYJovWtAj4ubgENbNMdC7xD2JNRJEgGQxpb1m7AkrKyrqtlYxCSSBQ2RXv4rbo4RISVSKVITPPLhYQcAe-fwa-kX540GLU89kzNi2C6ED4A5xwVrvzO4xyqz73ooEQ3hBQjAp9q8PJDce8Hh59gPktbLyJJLpo1CjMOjFbpcn61_gGZ8EBA-cZUvDO3IlrUx1sg9G6eHnG0aGCZTolEstyBVsgpFCcFQ23COWkI-WxawJMhWHpo9YRp89u0U7jT_4EmmVoqj1wkjtqcQSUPFNVafrY_veIatlx3Dkz3N2OOMFKVrnFnc">
                 CV </a></li> </ul>
           </div> 
           <div className="head">
-            <h2 id="SKILLS" >SKILLS</h2>  
+            <h2  >SKILLS</h2>  
             <h4>Programming Languages & Tools:</h4>
             <ul class="list-inline list-icons">
               <li class="list-inline-item">
@@ -126,10 +128,10 @@ export default class App extends Component {
               <li>&#10147;Time Managment</li>
               <li>&#10147;Negotiation</li>
               <li>&#10147;Problem Solving</li>
-              <li>&#10147;Flexible, Adaptive & Open to Constructive Criticism</li>
+              <li id="PROJECTS">&#10147;Flexible, Adaptive & Open to Constructive Criticism</li>
             </ul>
           </div>
-          <div id="PROJECTS" className="head">
+          <div  className="head">
             <h2>PROJECTS</h2>
             <h4>Online.com</h4>
             <p>An e-commerce delivery site that is designed to let the user place an order as a
@@ -164,11 +166,11 @@ export default class App extends Component {
                 <i  style={{  border: "solid  rgba(3, 18, 233, 0.301) 4px"}} class="devicon-react-original-wordmark colored"></i>&nbsp;
               CSS 3 <i style={{  border: "solid  rgba(3, 18, 233, 0.301) 4px"}} class="devicon-css3-plain-wordmark colored"></i>&nbsp;
               Bootstrap <i  style={{  border: "solid  rgba(3, 18, 233, 0.301) 4px"}}class="devicon-bootstrap-plain-wordmark colored"></i></p>
-              <a href="https://github.com/profissional-coder/Rentello-website"><button class="btn btn-secondary"  >
+              <a id="EDUCATION" href="https://github.com/profissional-coder/Rentello-website"><button class="btn btn-secondary"  >
                 <i style={{  border: "solid  rgba(3, 18, 233, 0.301) 4px"}} className="	far fa-file-code"></i>  Project Code  click me !</button></a>
             </div>
           </div>
-          <div id="EDUCATION" className="head">
+          <div  className="head">
             <h2> EDUCATION </h2>
             <h4>Meraki Academy</h4>
             <p>We learned how to become a
@@ -177,9 +179,9 @@ export default class App extends Component {
                 node js, express js as as back end</p>
             <h4>Al- Balqa' Applied University</h4>
             <p>Bachelor of Science in Mechanical Engineering</p>
-            <p>Graduated at 2018</p>
+            <p id="INTERESTS">Graduated at 2018</p>
           </div >
-          <div id="INTERESTS" className="head" >
+          <div  className="head" >
             <h2>INTERESTS & REFERENCEC</h2>
             <h4>INTERESTS</h4>
             <p>&#10147; Reading <i class="fas fa-book" style={{ color: "black" }}></i></p>
@@ -197,9 +199,10 @@ export default class App extends Component {
             <p> <span className=" bg-dark text-primary"> BESSLAN BZADOUGH  :</span>
               <span className="text-secondary">(+962)  775 693 705  besslan.bazadough@gmail.com </span>
               <span className="text-secondary">/Insructor in Meraki Academy </span> </p>
-            <p> <span className=" bg-dark text-primary"> Bader Sabbagh :</span><span className="text-secondary"> (+962)  782 659 211  </span></p>
+            <p> <span className=" bg-dark text-primary"> Bader Sabbagh :</span><span className="text-secondary"> (+962)  782 659 211  </span>
+            <span className="text-secondary">/Manager in modanisa </span></p>
             <p><span className=" bg-dark text-primary">Mohammed Qassas :</span>
-              <span className="text-secondary"> (+962)  798 541 685 </span> </p>
+              <span className="text-secondary"> (+962)  798 541 685 </span> <span className="text-secondary">/Account-manager in modanisa </span></p>
 
 
           </div>
---
+++ src/PS.js
@@ -0,0 +1,6 @@
+const myFunction=(input)=>{
+if(!(input%input===0)){
+return "yes"
+}
+return "No"
+}
